3c76fd28c7da873fe4835c6181aa40c955d2b439
Manzil <shresthamanxil@gmail.com>
2023-08-01 13:37:29 +0545

    simple calender with any date

---
+++ Calender.py
@@ -0,0 +1,8 @@
+from calendar import*
+year = int(input('Enter Year: '))
+print(calendar(year, 2, 1, 8, 3))
+
+#2 = 2 character for days(Mo, Tu, etc)
+#1 = 1 line(row) for each week
+#8 = 8 rows for each month
+#3 = 3 coloumns for all months of the year
