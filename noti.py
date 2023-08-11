a1f87b58668d2001a78067de2c3bd1de2a81839e
Manzil <shresthamanxil@gmail.com>
2023-08-11 09:10:08 +0545

    modified notification

--- noti.py
+++ noti.py
@@ -1,10 +1,10 @@
 from plyer import notification
 import time
-if __name__ =='__main__':
+if __name__ == '__main__':
     while True:
         notification.notify(
-            title = 'Stay Focused. You dont need that',
-            message = 'Yeah relax take notes Dont you relapse stay focused You dont need that',
-            app_icon = 'D:/Chrome Downloads/norel',
-            timeout = 5,
-            )
+            title='Stay Focused. You dont need that',
+            message='Yeah relax take notes Dont you relapse stay focused You dont need that',
+            app_icon='D:/Chrome Downloads/norel',
+            timeout=5,
+        )
