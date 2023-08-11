5fdb61992f8bad3ce75f8642674616eb6b71aef9
Manzil <shresthamanxil@gmail.com>
2023-08-11 09:12:00 +0545

    modified notification

--- ytdownload.py
+++ ytdownload.py
@@ -3,10 +3,10 @@ from pytube import YouTube
 link = 'https://www.youtube.com/watch?v=PAfG8P1cgiI'
 
 yt = YouTube(link)
-#print(yt.title) #prints the title
+# print(yt.title) #prints the title
 
 # videos = yt.streams.filter(only_audio = True) #shows only audio formats
-videos = yt.streams.all() #shows all downloadable format
+videos = yt.streams.all()  # shows all downloadable format
 vid = list(enumerate(videos))
 for i in vid:
     print(i)
@@ -17,8 +17,8 @@ print('downloaded succesfully')
 
 # ****** to download a playlist********
 
-#from pytube import Playlist
-#py = Playlist('paste the url here')
-#print(f'downloading: {py.title}')
-#for video in py.videos:
-    #video.streams.first().download
+# from pytube import Playlist
+# py = Playlist('paste the url here')
+# print(f'downloading: {py.title}')
+# for video in py.videos:
+# video.streams.first().download
